6654049ebe09a9762a4dff72b8558fb7dd8eec5e
Shadab Mehdi <ausshadu@gmail.com>
2022-12-15 13:39:44 +0400

    Added Heros Mock Data

---
+++ src/MockData/heros.mock.ts
@@ -0,0 +1,4 @@
+import { Hero } from '../models/hero.model';
+import * as HerosRawData from './heros.json';
+
+export const HerosMockData: Hero[] = HerosRawData as Hero[];
--- src/Screens/HeroOfTheDay.tsx
+++ src/Screens/HeroOfTheDay.tsx
@@ -1,7 +1,12 @@
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
 export default function HeroOfTheDay() {
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <View>
       <Text>HeroOfTheDay</Text>
